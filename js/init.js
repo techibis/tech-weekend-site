fcb4fc1e99df6803cddedf90b57cfdbe279004e1
mauro <maurop123@gmail.com>
2016-08-10 12:07:21 -0400

    init v2

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax({
+      height: '1000px'
+    });
+
+    $(document).ready(function(){
+      $('.slider').slider({
+        height: '600px',
+        full_width: true});
+    });
+
+    $('.carousel.carousel-slider').carousel({full_width: true});
+  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
